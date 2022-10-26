33d43d85851ffc4d1248aeba23d5314a6ad47f9d
Wendster21 <80414578+Wendster21@users.noreply.github.com>
2022-10-26 15:19:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+[
+   {
+    "name": "Love Confession",
+     "artist": "Jay Chou",
+     "year": 2016,
+     "genre": "Mando Pop/Pop"
+   },
+    {
+    "name": "Mojito",
+     "artist": "Jay Chou",
+     "year": 2022,
+     "genre": "Mando Pop/Pop"
+   },
+   {
+    "name": "Missionary",
+     "artist": "Ozi",
+     "year": 2021,
+     "genre": "R and B/Soul"
+   },
+ {
+    "name": "0.03",
+     "artist": "Jay Chou",
+     "year": 2021,
+     "genre": "R and B/Soul"
+   },
+    {
+    "name": "First Class",
+     "artist": "Jack Harlow",
+     "year": 2022,
+     "genre": "Hip Hop/Rap"
+   },
+    {
+    "name": "Love Story",
+     "artist": "Taylor Swift",
+     "year": 2008,
+     "genre": "Country Pop"
+   },
+ {
+    "name": "Don't Let Me Down",
+     "artist": "Chainsmokers",
+     "year": 2016,
+     "genre": "EDM/Trap"
+   },
+{
+    "name": "How You Like That",
+     "artist": "Blackpink",
+     "year": 2020,
+     "genre": "Kpop"
+   },
+{
+    "name": "Love Shot",
+     "artist": "EXO",
+     "year": 2018,
+     "genre": "Kpop"
+   },
+{
+    "name": "Something about LA",
+     "artist": "Eric Chou",
+     "year": 2019,
+     "genre": "Mando Pop/Pop"
+   },
+{
+    "name": "Whistle",
+     "artist": "Blackpink",
+     "year": 2016,
+     "genre": "Kpop"
+   },
+{
+    "name": "Paris in the Rain",
+     "artist": "Lauv",
+     "year": 2018,
+     "genre": "R & B/Soul"
+   }
+   ]
